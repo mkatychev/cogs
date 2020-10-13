5d38dad6bb6588a68061ac588e138d308029288b
Mikhail Katychev <mkatych@gmail.com>
2020-10-13 16:36:58 -0500

    added output type

--- cmd/cogs/main.go
+++ cmd/cogs/main.go
@@ -7,30 +7,57 @@ import (
 
 	"github.com/bestowinc/cogs"
 	"github.com/docopt/docopt-go"
+	"github.com/pelletier/go-toml"
 	logging "gopkg.in/op/go-logging.v1"
+	"gopkg.in/yaml.v3"
 )
 
 func main() {
-	usage := `Usage:
-  cogs generate <env> <cog-file>`
+	usage := `
+COGS
+
+Usage:
+  cogs generate <env> <cog-file> [--out=<type>]
+
+Options:
+  --output=<type>  Configuration output type [default: json].
+`
 
 	opts, _ := docopt.ParseArgs(usage, os.Args[1:], "0.1")
 	var conf struct {
 		Generate bool
 		Env      string
 		File     string `docopt:"<cog-file>"`
+		Output   string `docopt:"--out"`
+	}
+
+	opts.Bind(&conf)
+
+	if conf.Output == "" {
+		conf.Output = "json"
 	}
 
 	logging.SetLevel(logging.WARNING, "yq")
-
-	opts.Bind(&conf)
 	switch {
 	case conf.Generate:
 		cfgMap, err := cogs.Generate(conf.Env, conf.File)
 		if err != nil {
 			panic(err)
 		}
-		output, _ := json.MarshalIndent(cfgMap, "", "  ")
+		var output []byte
+		switch conf.Output {
+		case "json":
+			output, err = json.MarshalIndent(cfgMap, "", "  ")
+		case "yaml":
+			output, err = yaml.Marshal(cfgMap)
+		case "toml":
+			output, err = toml.Marshal(cfgMap)
+		default:
+			panic("invalid arg: --out=%" + conf.Output)
+		}
+		if err != nil {
+			panic(err)
+		}
 		fmt.Println(string(output))
 	}
 }
